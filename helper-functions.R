1fd942027c47416e2af3c4d2bc6d40d9e2082d48
Shannon Carter <skcarter25@gmail.com>
2021-03-19 13:40:09 -0500

    making plots for blog

--- helper-functions.R
+++ helper-functions.R
@@ -7,7 +7,7 @@ make_bar_chart <- function(.data, .x_var, .y_var, .x_axis_name, .title) {
   
   ggplot(freq_first_last, aes(x = !!x_var, y = reorder(!!y_var, !!x_var))) +
     geom_bar(stat = "identity", fill = "#5c9ad2", size = 2) +
-    geom_text(aes(label = paste0(round(!!x_var), "%")), hjust=1, family="Roboto", fontface="bold") +
+    geom_text(aes(label = paste0(round(!!x_var), "%")), hjust=1, family="Roboto", fontface="bold", color = "white") +
     labs(
       title = .title,
       x = .x_axis_name,
@@ -32,7 +32,7 @@ make_colors_chart <- function(.data, .filter_name) {
     filter(name == .filter_name) %>% 
     ggplot(aes(x = index, y = name, fill = value)) +
     geom_tile(color = "white", size = 1) + 
-    geom_text(aes(label=value), family="Roboto", fontface="bold") +
+    geom_text(aes(label=value), family="Roboto", fontface="bold", color = "white") +
     scale_fill_manual(values = c(`1`="#ff0000", `2`="#f59035", `3`="#ffff00", 
                                  `4`="#149414", `5`="#5c9ad2", `6`="#2b5bb0", `7`="#663399", `8`="#ff69b4")) + 
     theme_void() +
